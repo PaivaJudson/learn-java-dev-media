47d3b78b9e6ed6adeeadc103c53a233c39e22627
Judson Paiva <judson.paiva@hotmail.com>
2021-12-13 01:48:27 +0100

    Uso de variáveis no Java

---
+++ src/modulo/dois/Main.java
@@ -0,0 +1,27 @@
+package modulo.dois;
+
+public class Main {
+
+    public static void main(String[] args) {
+        final float pi = 3.146f;
+        final double e;
+
+        e = 1.234e2;
+
+        String nome = "Estevão Dias";
+        String dataAniversario = "23/05/2000";
+
+        int lote = 354556;
+        boolean aprovado;
+
+        aprovado = true;
+
+        System.out.println(pi);
+        System.out.println(e);
+        System.out.println(nome);
+        System.out.println(dataAniversario);
+        System.out.println(lote);
+        System.out.println(aprovado);
+
+    }
+}
